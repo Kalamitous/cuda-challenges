cd26a076318d7ba5ebd32ad4c9440f91271872a0
David Ning <dning2091@gmail.com>
2025-10-12 21:53:59 -0400

    optimize prefix sum

--- leetgpu/prefix_sum.cu
+++ leetgpu/prefix_sum.cu
@@ -1,20 +1,79 @@
 #include <cuda_runtime.h>
 
-// naive solution; todo: hillis steele scan
-__global__ void prefix_sum(const float* input, float* output, int N) {
+// Hillis–Steele inclusive scan w/ double-buffering & host-recursive grid-level accumulation
+// todo: warp-level accumulation before block-level accumulation
+
+const int threads = 1024;
+
+__global__ void prefix_sum(const float* input, float* output, float* partial_sums, int N) {
+    __shared__ float buf1[threads];
+    __shared__ float buf2[threads];
+
+    float* input_s = buf1;
+    float* output_s = buf2;
+    
     int idx = blockIdx.x * blockDim.x + threadIdx.x;
-    if (idx < N) {
-        float sum = 0.0f;
-        for (int i = 0; i < idx + 1; ++i) {
-            sum += input[i];
+
+    input_s[threadIdx.x] = (idx < N) ? input[idx] : 0.0f;
+    __syncthreads();
+
+    for (int stride = 1; stride <= blockDim.x / 2; stride *= 2) {
+        if (threadIdx.x >= stride) {
+            output_s[threadIdx.x] = input_s[threadIdx.x] + input_s[threadIdx.x - stride];
+        } else {
+            output_s[threadIdx.x] = input_s[threadIdx.x];
         }
-        output[idx] = sum;
+        __syncthreads();
+
+        float* tmp = input_s;
+        input_s = output_s;
+        output_s = tmp;
     }
+
+    if (partial_sums && threadIdx.x == blockDim.x - 1) {
+        partial_sums[blockIdx.x] = input_s[threadIdx.x];
+    }
+
+    if (idx < N) {
+        output[idx] = input_s[threadIdx.x];
+    }
+}
+
+__global__ void add_block_prefix_sum(float* output, const float* block_prefix_sums, int N) {
+    int idx = blockIdx.x * blockDim.x + threadIdx.x;
+    if (blockIdx.x == 0 || idx >= N) return;
+    
+    __shared__ float prefix_sum;
+    if (threadIdx.x == 0) {
+        prefix_sum = block_prefix_sums[blockIdx.x - 1];
+    }
+    __syncthreads();
+
+    output[idx] += prefix_sum;
+}
+
+void recurse(const float* input, float* output, int N) {
+    int blocks = (N + threads - 1) / threads;
+
+    if (blocks == 1) {
+        return prefix_sum<<<1, threads>>>(input, output, nullptr, N);
+    }
+
+    float* block_sums;
+    cudaMalloc(&block_sums, blocks * sizeof(float));
+    prefix_sum<<<blocks, threads>>>(input, output, block_sums, N);
+
+    float* block_prefix_sums;
+    cudaMalloc(&block_prefix_sums, blocks * sizeof(float));
+    recurse(block_sums, block_prefix_sums, blocks);
+
+    add_block_prefix_sum<<<blocks, threads>>>(output, block_prefix_sums, N);
+
+    cudaFree(block_sums);
+    cudaFree(block_prefix_sums);
 }
 
 // input, output are device pointers
 extern "C" void solve(const float* input, float* output, int N) {
-    int threads = 256;
-    int blocks = (N + threads - 1) / threads;
-    prefix_sum<<<blocks, threads>>>(input, output, N);
+    recurse(input, output, N);
 } 
